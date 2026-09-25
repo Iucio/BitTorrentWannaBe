5f13107f97d8a50445893377e05b8173f73471a2
luizzeduardo <le_miranda@id.uff.br>
2026-09-25 13:55:46 -0300

    feat: interface do ler_announce

--- Tracker/cpp/src/url.hpp
+++ Tracker/cpp/src/url.hpp
@@ -3,6 +3,9 @@
 #include <map>
 #include <optional>
 #include <string>
+#include <variant>
+
+#include "modelos.hpp"
 
 namespace tracker {
 
@@ -11,5 +14,10 @@ std::optional<std::string> percent_decode(const std::string& s);
 
 // "a=1&b=2" -> {a: 1, b: 2}
 std::optional<std::map<std::string, std::string>> parse_query(const std::string& query);
+
+// Monta o AnnounceRequest ou devolve a mensagem de erro.
+// O parâmetro ip da url tem prioridade sobre ip_remetente.
+std::variant<AnnounceRequest, std::string> ler_announce(const std::string& query,
+                                                        const std::string& ip_remetente);
 
 } // namespace tracker
